850f7ac9f50562d6bc52aab4ac1b7e24544a1d06
Tom Hanover <thomas.hanover@gmail.com>
2024-07-21 17:41:42 +0000

    checkpoint_1_07_21

--- lambda/controllers/transaction.ts
+++ lambda/controllers/transaction.ts
@@ -2,28 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { DynamoDB, PutItemInput } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { randomUUID } from "crypto";
-
-export interface TransactionInput {
-  [index: string]: unknown;
-  id?: string;
-  description: string;
-  date: string;
-  amount: number;
-  bank: string;
-  cardName: string;
-}
-
-interface Transaction {
-  PK: string;
-  SK: string;
-  id: string;
-  description: string;
-  date: string;
-  amount: number;
-  dateAdded: string;
-  bank: string;
-  cardName: string;
-}
+import { Transaction, TransactionInput } from "../types/Transaction";
 
 export class TransactionController {
   private event: APIGatewayProxyEvent;
@@ -34,46 +13,60 @@ export class TransactionController {
   async createTransaction(): Promise<APIGatewayProxyResult> {
     const { body } = this.event;
     if (!body) return this.sendFail("invalid request");
-    const { id, description, amount, date, bank, cardName } = JSON.parse(
-      body
-    ) as TransactionInput;
+    const parsedBody = JSON.parse(body);
+    const transactionInputs: TransactionInput[] = parsedBody.transactions
+      ? parsedBody.transactions
+      : [parsedBody];
 
-    const dynamoClient = new DynamoDB({
-      region: "us-east-1",
+    const successfulPuts: Transaction[] = [];
+    const failures: string[] = [];
+    transactionInputs.map(async (transactionInput) => {
+      const { id, description, amount, date, bank, cardName } =
+        transactionInput;
+
+      const dynamoClient = new DynamoDB({
+        region: "us-east-1",
+      });
+
+      const newTransaction: Transaction = {
+        PK: `DESCRIPTION#${description}`,
+        SK: `DATE#${date}`,
+        id: id ?? randomUUID(),
+        description,
+        amount,
+        date,
+        bank,
+        cardName,
+        dateAdded: Date.now().toString(),
+      };
+
+      const transactionParams: PutItemInput = {
+        Item: marshall(newTransaction),
+        TableName: process.env.TABLE_NAME,
+      };
+
+      try {
+        await dynamoClient.putItem(transactionParams);
+        successfulPuts.push(newTransaction);
+      } catch (err) {
+        failures.push(
+          `Failed to put new transaction in the table!\n Transaction: ${newTransaction}.\n reason: ${err}`
+        );
+        console.log(err);
+      }
     });
 
-    const newTransaction: Transaction = {
-      PK: `DESCRIPTION#${description}`,
-      SK: `DATE#${date}`,
-      id: id ?? randomUUID(),
-      description,
-      amount,
-      date,
-      bank,
-      cardName,
-      dateAdded: Date.now().toString(),
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        createdTransactions: successfulPuts,
+        ...(failures && { failures }),
+      }),
     };
-
-    const transactionParams: PutItemInput = {
-      Item: marshall(newTransaction),
-      TableName: process.env.TABLE_NAME,
-    };
-
-    try {
-      await dynamoClient.putItem(transactionParams);
-      return {
-        statusCode: 201,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ newTransaction }),
-      };
-    } catch (err) {
-      console.log(err);
-      return this.sendFail("something went wrong");
-    }
   }
-
   sendFail(message: string): APIGatewayProxyResult {
     return {
       statusCode: 400,
